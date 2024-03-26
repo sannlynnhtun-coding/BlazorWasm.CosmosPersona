ab73b9bf5bbc8f0606786c68d2e8a13e8154ae85
mgchit <chitlunh@gmail.com>
2024-03-27 01:20:36 +0630

    [Inprogress] Project Farme

---
+++ BlazorWasm.CosmosPersona/Models/AnswerModel.cs
@@ -0,0 +1,101 @@
+﻿namespace BlazorWasm.CosmosPersona.Models;
+
+public class AnswerModel
+{
+    public ISTJ ISTJ { get; set; }
+    public ISFJ ISFJ { get; set; }
+    public INFJ INFJ { get; set; }
+    public INTJ INTJ { get; set; }
+    public ISTP ISTP { get; set; }
+    public ISFP ISFP { get; set; }
+    public INFP INFP { get; set; }
+    public INTP INTP { get; set; }
+    public ESTP ESTP { get; set; }
+    public ESFP ESFP { get; set; }
+    public ENFP ENFP { get; set; }
+    public ENTP ENTP { get; set; }
+    public ESTJ ESTJ { get; set; }
+    public ESFJ ESFJ { get; set; }
+    public ENFJ ENFJ { get; set; }
+    public ENTJ ENTJ { get; set; }
+}
+
+public class ISTJ
+{
+    public string image { get; set; }
+}
+
+public class ISFJ
+{
+    public string image { get; set; }
+}
+
+public class INFJ
+{
+    public string image { get; set; }
+}
+
+public class INTJ
+{
+    public string image { get; set; }
+}
+
+public class ISTP
+{
+    public string image { get; set; }
+}
+
+public class ISFP
+{
+    public string image { get; set; }
+}
+
+public class INFP
+{
+    public string image { get; set; }
+}
+
+public class INTP
+{
+    public string image { get; set; }
+}
+
+public class ESTP
+{
+    public string image { get; set; }
+}
+
+public class ESFP
+{
+    public string image { get; set; }
+}
+
+public class ENFP
+{
+    public string image { get; set; }
+}
+
+public class ENTP
+{
+    public string image { get; set; }
+}
+
+public class ESTJ
+{
+    public string image { get; set; }
+}
+
+public class ESFJ
+{
+    public string image { get; set; }
+}
+
+public class ENFJ
+{
+    public string image { get; set; }
+}
+
+public class ENTJ
+{
+    public string image { get; set; }
+}
---
+++ BlazorWasm.CosmosPersona/Models/QuestionModel.cs
@@ -0,0 +1,129 @@
+﻿namespace BlazorWasm.CosmosPersona.Models
+{
+    public class QuestionModel
+    {
+        public string question { get; set; }
+        public string image { get; set; }
+        public Answers answers { get; set; }
+    }
+
+    public class Answers
+    {
+        public F F { get; set; }
+        public T T { get; set; }
+        public J J { get; set; }
+        public P P { get; set; }
+        public S S { get; set; }
+        public N N { get; set; }
+        public I I { get; set; }
+        public E E { get; set; }
+        public K K { get; set; }
+        public U U { get; set; }
+    }
+
+    public class F
+    {
+        public string text { get; set; }
+        public Scores scores { get; set; }
+    }
+
+    public class Scores
+    {
+        public int T { get; set; }
+        public string F { get; set; }
+    }
+
+    public class T
+    {
+        public string text { get; set; }
+        public Scores1 scores { get; set; }
+    }
+
+    public class Scores1
+    {
+        public string T { get; set; }
+        public int F { get; set; }
+    }
+
+    public class J
+    {
+        public string text { get; set; }
+        public Scores2 scores { get; set; }
+    }
+
+    public class Scores2
+    {
+        public int P { get; set; }
+        public string J { get; set; }
+    }
+
+    public class P
+    {
+        public string text { get; set; }
+        public Scores3 scores { get; set; }
+    }
+
+    public class Scores3
+    {
+        public string P { get; set; }
+        public int J { get; set; }
+    }
+
+    public class S
+    {
+        public string text { get; set; }
+        public Scores4 scores { get; set; }
+    }
+
+    public class Scores4
+    {
+        public string S { get; set; }
+        public int N { get; set; }
+    }
+
+    public class N
+    {
+        public string text { get; set; }
+        public Scores5 scores { get; set; }
+    }
+
+    public class Scores5
+    {
+        public int S { get; set; }
+        public string N { get; set; }
+    }
+
+    public class I
+    {
+        public string text { get; set; }
+        public Scores6 scores { get; set; }
+    }
+
+    public class Scores6
+    {
+        public object I { get; set; }
+        public object E { get; set; }
+    }
+
+    public class E
+    {
+        public string text { get; set; }
+        public Scores7 scores { get; set; }
+    }
+
+    public class Scores7
+    {
+        public object I { get; set; }
+        public object E { get; set; }
+    }
+
+    public class K
+    {
+        public string text { get; set; }
+    }
+
+    public class U
+    {
+        public string text { get; set; }
+    }
+}
